1e6b94485c722845e48a6292155dda4c4efe9340
Patricia Costa <patriciabscosta2001@gmail.com>
2023-05-03 13:37:36 +0100

    update

---
+++ react_site/src/components/dashboard.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import './styles/homepage.css';
+import { useNavigate } from "react-router-dom";
+
+export const Dashboard = () => {
+
+    let navigate = useNavigate();
+
+    return (
+        <div id='body_dashboard'>
+           
+
+
+        </div>
+    )
+}
