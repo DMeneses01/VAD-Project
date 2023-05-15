c383e65cccec110f9b6f5f30fed5d3175a17066f
Duarte Meneses <93008941+DMeneses01@users.noreply.github.com>
2023-05-15 19:20:24 +0100

    Update graphs of home and country

--- dash_graphs/with_without.py
+++ dash_graphs/with_without.py
@@ -23,6 +23,24 @@ athlete_results = athlete_results.drop(columns=['result_id','athlete','athlete_i
 
 athlete_results['edition'] = athlete_results['edition'].str.split().str[0].astype(int)
 
+athlete_results = athlete_results[athlete_results["country_noc"].str.contains("MIX")==False] # Impossivel dividir
+athlete_results = athlete_results[athlete_results["country_noc"].str.contains("EUN")==False] # Impossivel dividir
+athlete_results = athlete_results[athlete_results["country_noc"].str.contains("IOA")==False] # Impossivel dividir
+athlete_results = athlete_results[athlete_results["country_noc"].str.contains("BOH")==False]    # Eslováquia + Republica Checa
+athlete_results = athlete_results[athlete_results["country_noc"].str.contains("ANZ")==False]    # Australia + Nova Zelandia + Nova Guine + partes da Indonesia
+athlete_results = athlete_results[athlete_results["country_noc"].str.contains("TCH")==False]    # Eslováquia + Republica Checa
+athlete_results = athlete_results[athlete_results["country_noc"].str.contains("YUG")==False] # Bosnia + Croacia + Macedonia + Montenegro + Eslovenia + Servia
+athlete_results = athlete_results[athlete_results["country_noc"].str.contains("URS")==False] # Russia + Letonia + Lituania + Estonia + Georgia + Armenia + Azerbaijao + Bielorrussia + Cazaquistao + Moldavia + Quirguistao + Tajiquistao + Turquemenistao + Ucrania + Usbequistao
+athlete_results = athlete_results[athlete_results["country_noc"].str.contains("UAR")==False]  # Egypt + Syria + Faixa de Gaza
+athlete_results = athlete_results[athlete_results["country_noc"].str.contains("WIF")==False] # Antigua + Barbados + Cayman Islands + Dominica + Grenada + Jamaica + Montserrat + St Christopher-Nevis-Anguilla + Saint Lucia	+ St Vincent and the Grenadines	+ Trinidad and Tobago + Turks and Caicos Islands	
+
+athlete_results['country_noc'] = athlete_results['country_noc'].replace(['FRG'], 'GER')
+athlete_results['country_noc'] = athlete_results['country_noc'].replace(['GDR'], 'GER')
+
+athlete_results['country_noc'] = athlete_results['country_noc'].replace(['HKG'], 'CHN')
+
+athlete_results['country_noc'] = athlete_results['country_noc'].replace(['ROC'], 'RUS')
+
 
 repeat = []
 i=0
